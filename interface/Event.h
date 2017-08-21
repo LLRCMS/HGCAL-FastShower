33c018944f9148c3b666031291983c8c7673ba26
Marina Beguin <marina.beguin@cern.ch>
2017-08-21 11:41:05 +0200

    add the cell getter for the timing improvment of the OutputService

--- interface/Event.h
+++ interface/Event.h
@@ -3,6 +3,8 @@
 
 
 #include <unordered_map>
+#include "Cell.h"
+
 
 class Event {
 
@@ -19,6 +21,7 @@ class Event {
         void fillGenPhi(uint32_t, double);
         void fillPDGid(uint32_t, int);
         void fillThick(uint32_t, int);
+        void fillCells(uint32_t, Cell&);
         void setnPart(uint32_t);
         void clear();
 
@@ -31,12 +34,14 @@ class Event {
         const std::unordered_map<uint32_t, double>& gen_phi() const {return generated_phi_;}
         const std::unordered_map<uint32_t, double>& pdg_id() const {return pdgid_;}
         const std::unordered_map<uint32_t, double>& thick() const {return thick_;}
+        const std::unordered_map<uint32_t, Cell>& cells() const {return cells_;}
 
     private:
         uint32_t run_;
         uint32_t event_;
         uint32_t npart_;
         std::unordered_map<uint32_t, double> hits_;
+        std::unordered_map<uint32_t, Cell> cells_;
         std::unordered_map<uint32_t, double> generated_energy_;
         std::unordered_map<uint32_t, double> generated_eta_;
         std::unordered_map<uint32_t, double> generated_phi_;
--- src/Event.cpp
+++ src/Event.cpp
@@ -12,36 +12,32 @@ Event::Event(uint32_t run, uint32_t event): run_(run), event_(event) {
 
 void Event::fillHit(uint32_t id, double energy) {
     auto itr_insert = hits_.emplace(id, energy);
-    // if id already inserted, add the energy
+    // if id already inserted (noise and hit), add the energy
     if(!itr_insert.second) itr_insert.first->second += energy;
 }
 
 void Event::fillGenEn(uint32_t id, double energy) {
-    auto itr_insert = generated_energy_.emplace(id, energy);
-    // if id already inserted, add the energy
-    if(!itr_insert.second) itr_insert.first->second += energy;
+    generated_energy_.emplace(id, energy);
 }
 
 void Event::fillGenEta(uint32_t id, double eta) {
-    auto itr_insert = generated_eta_.emplace(id, eta);
-    // if id already inserted, add the eta
-    if(!itr_insert.second) itr_insert.first->second += eta;
+    generated_eta_.emplace(id, eta);
 }
 
 void Event::fillGenPhi(uint32_t id, double phi) {
-    auto itr_insert = generated_phi_.emplace(id, phi);
-    // if id already inserted, add the phi
-    if(!itr_insert.second) itr_insert.first->second += phi;
+    generated_phi_.emplace(id, phi);
 }
 
 void Event::fillPDGid(uint32_t id, int PDGid) {
-    auto itr_insert = pdgid_.emplace(id, PDGid);
-    // if id already inserted, add the phi
-    if(!itr_insert.second) itr_insert.first->second += PDGid;
+    pdgid_.emplace(id, PDGid);
 }
 
 void Event::fillThick(uint32_t id, int thick) {
     thick_.emplace(id, thick);
+}
+
+void Event::fillCells(uint32_t id, Cell& cell) {
+    cells_.emplace(id, cell);
 }
 
 void Event::setnPart(uint32_t part) {
