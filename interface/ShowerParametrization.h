d286d0f12dba8eaa880dfbf1c94113eaa680dc5f
mabeguin <marina.beguin@cern.ch>
2017-11-07 12:52:22 +0100

    Remove magic numbers and change indentation

--- interface/ShowerParametrization.h
+++ interface/ShowerParametrization.h
@@ -12,13 +12,16 @@
 
 using namespace std;
 
+const int NB_LAYERS = 52;
+const int NB_SI_THICKNESS = 3;
+
 class ShowerParametrization {
 
-    // Electromagnetic shower parametrization 
-    // values from G. Grindhammer et al., hep-ex/0001020
-    // adapted for HGCAL TP geometry
+// Electromagnetic shower parametrization 
+// values from G. Grindhammer et al., hep-ex/0001020
+// adapted for HGCAL TP geometry
 
-    public:
+  public:
 
     ShowerParametrization() {}
     ShowerParametrization(const Parameters::Shower& params);
@@ -29,39 +32,39 @@ class ShowerParametrization {
     double getMoliereRadius() const {return moliereRadius_;}
     double getInteractionLength() const {return interaction_length_;}
 
-    std::array<double,52>& getLayerProfile(int pdgid);
+    std::array<double, NB_LAYERS>& getLayerProfile(int pdgid);
 
     // transversal
     double r0_electro_(int klayer, int pdgid) {return (a0_electro + a1_electro*klayer + a2_electro*klayer*klayer)*r0layer_Electro_/tot_layer_Electro;}
     double r0_hadro_(int klayer, int pdgid) { return (a0_hadro + a1_hadro*klayer + a2_hadro*klayer*klayer)/tot_layer_Hadro;}
 
     // fluctuations
-    std::array<double,3> spotEnergy(int pdgid);
+    std::array<double, NB_SI_THICKNESS> spotEnergy(int pdgid);
 
-    private:
-        // average medium parameters
-        std::array<double,52> radLength_;
-        std::map<int, std::vector<double>> layerProfile_;
-        std::array<double,52> partLayerProfile_;
+private:
+    // average medium parameters
+    std::array<double, NB_LAYERS> radLength_;
+    std::map<int, std::vector<double>> layerProfile_;
+    std::array<double, NB_LAYERS> partLayerProfile_;
 
-        double moliereRadius_;   // in cm 
-        double interaction_length_;  // in cm
+    double moliereRadius_;   // in cm 
+    double interaction_length_;  // in cm
 
-        int tot_layer_Electro;
-        int tot_layer_Hadro;
+    int tot_layer_Electro;
+    int tot_layer_Hadro;
 
-        // transverse parametrisation
-        double r0layer_Electro_;
-        double a0_hadro;
-        double a1_hadro;
-        double a2_hadro;
-        double a0_electro;
-        double a1_electro;
-        double a2_electro;
+    // transverse parametrisation
+    double r0layer_Electro_;
+    double a0_hadro;
+    double a1_hadro;
+    double a2_hadro;
+    double a0_electro;
+    double a1_electro;
+    double a2_electro;
 
-        // for fluctuation
-        std::map<int, std::vector<double>> alpha_;
-        std::array<double,3> alphasquare_; // the stochastic coefficient in GeV^1/2
+    // for fluctuation
+    std::map<int, std::vector<double>> alpha_;
+    std::array<double, NB_SI_THICKNESS> alphasquare_; // the stochastic coefficient in GeV^1/2
 };
 
 #endif
