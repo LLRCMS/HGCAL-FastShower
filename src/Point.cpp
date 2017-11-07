0fe4c9b9ffd19d5adedc718769d935e264de5ba5
mabeguin <marina.beguin@cern.ch>
2017-11-07 16:10:38 +0100

    Add the possibility to use Standalone or with CMSSW

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,6 +1,13 @@
-#include "Point.h"
 
-Point::Point(float x, float y) {
+#ifdef STANDALONE
+#include "Point.h"
+#else
+#include "HGCalSimulation/FastShower/interface/Point.h"
+#endif
+
+Point::
+Point(float x, float y)
+{
     this->x = x;
     this->y = y;
 }
--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -1,13 +1,21 @@
-#include "Rectangle.h"
+
+
 #include <iostream>
+#ifdef STANDALONE
+#include "Rectangle.h"
+#else
+#include "HGCalSimulation/FastShower/interface/Rectangle.h"
+#endif
 
 //
 // Contructors
 //
 
-Rectangle::Rectangle(Point* topLeft, Point* bottomRight) {
-    this->topLeft = topLeft;
-    this->bottomRight = bottomRight;
+Rectangle::
+Rectangle(Point* topLeft, Point* bottomRight)
+{
+  this->topLeft = topLeft;
+  this->bottomRight = bottomRight;
 }
 
 Rectangle::~Rectangle() { }
@@ -16,28 +24,35 @@ Rectangle::~Rectangle() { }
 // Methods
 //
 
-bool Rectangle::contains(Point* p) {
-    return p->x >= topLeft->x
-        && p->x <= bottomRight->x
-        && p->y >= bottomRight->y
-        && p->y <= topLeft->y;
+bool Rectangle::
+contains(Point* p)
+{
+  return p->x >= topLeft->x
+      && p->x <= bottomRight->x
+      && p->y >= bottomRight->y
+      && p->y <= topLeft->y;
 }
 
-Point* Rectangle::getCenter() {
-    float dx = bottomRight->x - topLeft->x;
-    float dy = topLeft->y - bottomRight->y;
+Point* Rectangle::
+getCenter()
+{
+  float dx = bottomRight->x - topLeft->x;
+  float dy = topLeft->y - bottomRight->y;
 
-    return new Point(topLeft->x + (dx / 2), bottomRight->y + (dy / 2));
+  return new Point(topLeft->x + (dx / 2), bottomRight->y + (dy / 2));
 }
 
 //
 // Getters
 //
 
-Point* Rectangle::getTopLeft() {
-    return topLeft;
+Point* Rectangle::
+getTopLeft()
+{
+  return topLeft;
 }
 
-Point* Rectangle::getBottomRight() {
-    return bottomRight;
+Point* Rectangle::getBottomRight()
+{
+  return bottomRight;
 }
