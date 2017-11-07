969875fa827270440edff55aed038328f5708f5c
mabeguin <marina.beguin@cern.ch>
2017-11-07 16:01:26 +0100

    Add the standalone/CMSSW option and change all indentation in order to have the same in all files

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -1,124 +1,143 @@
-#include "Tree.h"
+
 #include <iostream>
 
-Tree::Tree(Rectangle* r, int levels) {
-    rectangle = r;
+#ifdef STANDALONE
+#include "Tree.h"
+#else
+#include "HGCalSimulation/FastShower/interface/Tree.h"
+#endif
 
-    cells = nullptr;
+Tree::
+Tree(Rectangle* r, int levels)
+{
+  rectangle = r;
+  cells = nullptr;
 
-    subdivide(levels);
+  subdivide(levels);
 }
 
-Tree::~Tree() {
-    if(cells != nullptr) {
-        delete cells;
-    }
+Tree::
+~Tree()
+{
+  if(cells != nullptr) {
+    delete cells;
+  }
 
-    delete rectangle;
+  delete rectangle;
 
-    delete nw;
-    delete sw;
-    delete ne;
-    delete se;
+  delete nw;
+  delete sw;
+  delete ne;
+  delete se;
 }
 
 //
 // Methods
 //
 
-bool Tree::empty() {
-    if (cells == nullptr)
-        return true;
-    else
-        return false;
+bool Tree::
+empty()
+{
+  if (cells == nullptr) return true;
+  else return false;
 }
 
-void Tree::addCell(Cell* c) {
-    cells->push_back(c);
+void Tree::
+addCell(Cell* c)
+{
+  cells->push_back(c);
 }
 
-int Tree::countCells() {
-    if(cells != nullptr) {
-        // std::cout << "This leaf has " << cells->size() << " elements" << std::endl;
-        return cells->size();
-    }
+int Tree::
+countCells()
+{
+  if(cells != nullptr) {
+    // std::cout << "This leaf has " << cells->size() << " elements" << std::endl;
+    return cells->size();
+  }
 
-    int cellCount = 0;
+  int cellCount = 0;
 
-    cellCount += nw->countCells();
-    cellCount += sw->countCells();
-    cellCount += ne->countCells();
-    cellCount += se->countCells();
+  cellCount += nw->countCells();
+  cellCount += sw->countCells();
+  cellCount += ne->countCells();
+  cellCount += se->countCells();
 
-    return cellCount;
+  return cellCount;
 }
 
-std::vector<Cell*>* Tree::getCells() {
-    return cells;
+std::vector<Cell*>* Tree::
+getCells()
+{
+  return cells;
 }
 
-Tree* Tree::getLeaf(float x, float y) {
-    Point* p = new Point(x, y);
-    return getLeaf(p);
+Tree* Tree::
+getLeaf(float x, float y)
+{
+  Point* p = new Point(x, y);
+  return getLeaf(p);
 }
 
-Tree* Tree::getLeaf(Point* p) {
-    if(cells != nullptr) {
-        return this;
-    }
+Tree* Tree::
+getLeaf(Point* p)
+{
+  if(cells != nullptr) return this;
 
-    if(nw->rectangle->contains(p)) {
-        // std::cout << "Going to subtree nw" << std::endl;
-        return nw->getLeaf(p);
-    }
+  if(nw->rectangle->contains(p)) {
+    // std::cout << "Going to subtree nw" << std::endl;
+    return nw->getLeaf(p);
+  }
 
-    if(sw->rectangle->contains(p)) {
-        // std::cout << "Going to subtree sw" << std::endl;
-        return sw->getLeaf(p);
-    }
+  if(sw->rectangle->contains(p)) {
+    // std::cout << "Going to subtree sw" << std::endl;
+    return sw->getLeaf(p);
+  }
 
-    if(ne->rectangle->contains(p)) {
-        // std::cout << "Going to subtree ne" << std::endl;
-        return ne->getLeaf(p);
-    }
+  if(ne->rectangle->contains(p)) {
+    // std::cout << "Going to subtree ne" << std::endl;
+    return ne->getLeaf(p);
+  }
 
-    if(se->rectangle->contains(p)) {
-        // std::cout << "Going to subtree se" << std::endl;
-        return se->getLeaf(p);
-    }
+  if(se->rectangle->contains(p)) {
+    // std::cout << "Going to subtree se" << std::endl;
+    return se->getLeaf(p);
+  }
 
-    // std::cout << "Looking for (" << p->x << ", " << p->y << ") "<< std::endl;
-    // std::cout << "topLeft is (" << rectangle->getTopLeft()->x << ", "
-    //     << rectangle->getTopLeft()->y << ")" << std::endl;
-    // std::cout << "BottomRight is (" << rectangle->getBottomRight()->x << ", "
-    //     << rectangle->getBottomRight()->y << ")" << std::endl;
-    throw std::string("Coordinates not found.");
+  // std::cout << "Looking for (" << p->x << ", " << p->y << ") "<< std::endl;
+  // std::cout << "topLeft is (" << rectangle->getTopLeft()->x << ", "
+  //     << rectangle->getTopLeft()->y << ")" << std::endl;
+  // std::cout << "BottomRight is (" << rectangle->getBottomRight()->x << ", "
+  //     << rectangle->getBottomRight()->y << ")" << std::endl;
+  throw std::string("Coordinates not found.");
 }
 
-void Tree::subdivide(int remainingLevels) {
-    if(remainingLevels == 0) {
-        cells = new std::vector<Cell*>;
-        return;
-    }
+void Tree::
+subdivide(int remainingLevels)
+{
+  if(remainingLevels == 0) {
+    cells = new std::vector<Cell*>;
+    return;
+  }
 
-    Point* center = rectangle->getCenter();
+  Point* center = rectangle->getCenter();
 
-    Rectangle* r_nw = new Rectangle(rectangle->getTopLeft(), center);
-    
-    Rectangle* r_sw = new Rectangle(
-        new Point(rectangle->getTopLeft()->x, center->y),
-        new Point(center->x, rectangle->getBottomRight()->y)
-    );
+  Rectangle* r_nw = new Rectangle(rectangle->getTopLeft(), center);
 
-    Rectangle* r_ne = new Rectangle(
-        new Point(center->x, rectangle->getTopLeft()->y),
-        new Point(rectangle->getBottomRight()->x, center->y)
-    );
+  Rectangle* r_sw = new Rectangle(
+    new Point(rectangle->getTopLeft()->x, center->y),
+    new Point(center->x, rectangle->getBottomRight()->y)
+  );
 
-    Rectangle* r_se = new Rectangle(center, rectangle->getBottomRight());
+  Rectangle* r_ne = new Rectangle(
+    new Point(center->x, rectangle->getTopLeft()->y),
+    new Point(rectangle->getBottomRight()->x, center->y)
+  );
 
-    nw = new Tree(r_nw, remainingLevels - 1);
-    sw = new Tree(r_sw, remainingLevels - 1);
-    ne = new Tree(r_ne, remainingLevels - 1);
-    se = new Tree(r_se, remainingLevels - 1);
+  Rectangle* r_se = new Rectangle(center, rectangle->getBottomRight());
+
+  nw = new Tree(r_nw, remainingLevels - 1);
+  sw = new Tree(r_sw, remainingLevels - 1);
+  ne = new Tree(r_ne, remainingLevels - 1);
+  se = new Tree(r_se, remainingLevels - 1);
 }
