6d607b9f8fea1a2c81084e900d38b21391022604
Marina Beguin <marina.beguin@cern.ch>
2018-01-02 10:33:58 +0100

    change the layer display

--- python/display_cfg.py
+++ python/display_cfg.py
@@ -4,4 +4,4 @@ display_events = 1
 # choose the layer that you want visualise in the output geometry histogram (note: the first layer
 #is 0). If you want disable the geometry histogram for quicker simulation put the display_layer
 # to -1.
-display_layer = 30
+display_layer = 11
